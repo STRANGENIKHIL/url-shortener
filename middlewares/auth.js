4a42381e5d72d11f084ab6966ee61580023b7922
STRANGENIKHIL <nikhilstrange4@gmail.com>
2026-05-22 23:09:53 +0530

    just used jwt how to implement it

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,9 @@ const {getUser} = require('../service/auth');
 async function restrictToLoggedInUsers(req,res,next){
     //console.log(req);
     const userUid = req.cookies.uid ?? null;
+    if(!userUid){
+        return res.redirect('/login');
+    }
     const user = getUser(userUid);
     if(!user){
         return res.redirect('/login');
